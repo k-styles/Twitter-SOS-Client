f2830bb25722ed5d06ba4459c8720674ff22ee3d
Namami Shanker <namami2011@gmail.com>
2021-07-20 15:59:27 +0530

    Create app.py file

---
+++ app.py
@@ -0,0 +1,11 @@
+""" Argument parser for command line arguments.
+"""
+from src.twitter_client import get_rules
+
+def parse_arguments(args):
+	if(args.start):
+		# TODO: Implement starting the app
+		pass
+	if(args.get_rules):
+		# TODO: Implement starting the app
+		pass
