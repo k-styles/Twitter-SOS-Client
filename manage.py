9a3bf0bc7e1e785f9336eb5eb325cf93f4149b45
Namami Shanker <namami2011@gmail.com>
2021-07-20 15:58:51 +0530

    Add argument parser

--- manage.py
+++ manage.py
@@ -1,2 +1,18 @@
-import arparse
+#!/usr/bin/env python3
 
+import argparse
+
+version = "0.10."
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-st",
+					"--start",
+					help="Start the application",
+					action="store_true")
+
+parser.add_argument("--getrules",
+					help="Get the current rules",
+					action="store_true")
+
+ARGV = parser.parse_args()
