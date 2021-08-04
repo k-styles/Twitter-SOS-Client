4e3377d558e89b6c799255458d019074566cce01
Jaishika <jaishika.korivi.19031@iitgoa.ac.in>
2021-08-04 13:04:21 +0530

    Add utilities.py and handle delete rules and add rules functions

--- main.py
+++ main.py
@@ -21,5 +21,19 @@ parser.add_argument("-da",
 		    			"--del_all_rules",
 		    			help="Delete all the rules",
 		    			action="store_true")
+
+parser.add_argument("-di",
+					"--del_id",
+					help ="delete by ID")
+
+parser.add_argument("-dv",
+					"--del_val",
+					help ="delete by value")
+
+parser.add_argument("-add",
+                    "--add_rule",
+                    help="Adds the rules",
+					action = "store_true")
+
 ARGV = parser.parse_args()
 parse_arguments(ARGV)
--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 
 from .twitter_client import TwitterStreamingClient
 from .config import BEARER_TOKEN, API_SECRET_KEY
+from .utilities import get_rule_from_input
 
 streamingClient = TwitterStreamingClient(BEARER_TOKEN, API_SECRET_KEY)
 
@@ -10,6 +11,17 @@ def parse_arguments(args):
 		streamingClient.stream_tweets()
 	if(args.getrules):
 		rules = streamingClient.get_rules()
-		print(rules)
+		return
 	if(args.del_all_rules):
 		streamingClient.delete_all_rules()
+	if (args.del_id):
+		ids_list = list(args.del_id.split(" "))
+		streamingClient.delete_rules_by_ids(ids_list)
+	if (args.del_val):
+		values_list = list(args.del_val.split(","))
+		for i in range(0,len(values_list)):
+			values_list[i] = values_list[i].strip()
+		streamingClient.delete_rules_by_values(values_list)
+	if(args.add_rule):
+		temp = get_rule_from_input()
+		streamingClient.add_rules(temp)
