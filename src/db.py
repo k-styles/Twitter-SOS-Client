ab2a56ea7af0d548d2214c2afbc5de2d88011e93
Namami Shanker <namami2011@gmail.com>
2021-08-19 16:17:26 +0530

    Consolidate mongo and redis in DB file

---
+++ src/db.py
@@ -0,0 +1,69 @@
+import logging
+from pymongo import MongoClient
+import redis
+import json
+
+# Setup the logger
+mongo_logger = logging.getLogger("mongo")
+mongo_logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('mongo.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+mongo_logger.addHandler(file_handler)
+
+redis_logger = logging.getLogger("redis")
+redis_logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('redis.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+redis_logger.addHandler(file_handler)
+
+from .config import MONGO_URI
+
+client = MongoClient(MONGO_URI)
+mongo_logger.info('Starting Atlas client')
+
+# Connect to the Server
+r = redis.Redis()
+redis_logger.info('Connecting to redis server')
+
+db = client.twitter_test_db
+tweets = db.tweets
+
+def insert_tweet(tweet):
+	try:
+		tweet_id = tweets.insert_one(tweet).inserted_id
+	except Exception as e:
+		logging.exception('Error inserting tweet')
+	else:
+		mongo_logger.debug('Added:tweet_id:'+str(tweet_id))
+
+def insert_bulk_tweets(tweets_list):
+	# TODO: Add bulk adding function
+	try:
+		tweet_id = tweets.insert_many(tweets_list).inserted_ids
+	except Exception as e:
+		mongo_logger.exception('Error inserting tweets list')
+	else:
+		mongo_logger.debug('Added:tweet_id:'+str(tweet_id))
+
+def add_tweet_to_cache(tweet):
+    r.lpush("tweets", json.dumps(tweet))
+    redis_logger.debug('Added:tweet_id'+str(tweet["data"]["id"]))
+
+def fetch_tweets_and_clear_cache():
+    tweets = list()
+    while(r.llen("tweets")!=0):
+        tweets.append(json.loads(r.lpop("tweets").decode("utf-8")))
+    return tweets
+
+def insert_cache_tweets_to_db():
+	tweets = fetch_tweets_and_clear_cache()
+	if(tweets):
+		insert_bulk_tweets(tweets)
