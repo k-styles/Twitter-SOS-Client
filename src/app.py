867fce27055f4ba6f46de6c38bd1e205ca11c5a5
Namami Shanker <namami2011@gmail.com>
2021-08-19 16:31:13 +0530

    Refactor app code

--- src/app.py
+++ src/app.py
@@ -1,31 +1,42 @@
 """ Argument parser for command line arguments."""
 
+import _thread
+
+from pprint import pprint
+
 from .twitter_client import TwitterStreamingClient
-from .config import BEARER_TOKEN, API_SECRET_KEY
-from .utilities import get_rule_from_input
+from .utilities import get_rule_from_input, scheduler
+from .db import insert_cache_tweets_to_db
+from .config import DELAY, BEARER_TOKEN, API_SECRET_KEY
 
 streamingClient = TwitterStreamingClient(BEARER_TOKEN, API_SECRET_KEY)
 
 def parse_arguments(args):
 	if(args.start):
-		streamingClient.stream_tweets()
+		_thread.start_new_thread(streamingClient.stream_tweets, ())
+		_thread.start_new_thread(scheduler, (DELAY, insert_cache_tweets_to_db))
+		while 1:
+			pass
     
 	if(args.getrules):
 		rules = streamingClient.get_rules()
-		return
+		pprint(rules, indent=4)
   
 	if(args.del_all_rules):
-		streamingClient.delete_all_rules()
+		resp = streamingClient.delete_all_rules()
+		pprint(resp, indent=4)
 
 	if (args.del_id):
 		ids_list = list(args.del_id.split(" "))
-		streamingClient.delete_rules_by_ids(ids_list)
+		resp = streamingClient.delete_rules_by_ids(ids_list)
+		pprint(resp, indent=4)
     
 	if (args.del_val):
 		values_list = list(args.del_val.split(","))
 		for i in range(0,len(values_list)):
 			values_list[i] = values_list[i].strip()
-		streamingClient.delete_rules_by_values(values_list)
+		resp = streamingClient.delete_rules_by_values(values_list)
+		pprint(resp, indent=4)
     
 	if(args.add_rule):
 		temp = get_rule_from_input()
